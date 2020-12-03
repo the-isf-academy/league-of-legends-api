413df70b1021fe3d15a33a47f4be2911aee11e06
Wesley <0111034@student.isf.edu.hk>
2020-12-03 12:46:19 +0800

    Created and completed routes for functions in bot_server
    What I changed
    Created all 3 routes for the 3 functions in bot_server
    
    What my goal was
    To create routes for all 3 functions

--- bot_server/bot_server.py
+++ bot_server/bot_server.py
@@ -1,9 +1,40 @@
 # bot_server.py
-# by 
+# by
 
 from flask import Flask, request
 from helpers import check_payload, parse_service_and_args_from, format_arguments
-from services import services_dict
+from services import services_dict, title, blurb, partype
 
 app = Flask(__name__)
 
+@app.route('/title', methods=['GET'])
+def title_wrapper():
+    data = request.get_json()
+    errors = check_payload(data, ["champion"])
+    if len(errors) > 0:
+        return {"errors": errors}, 400
+    champion = str(data['champion'])
+    ttl = title(champion)
+    return { "champion": ttl }
+
+
+@app.route('/blurb', methods=['GET'])
+def blurb_wrapper():
+    data = request.get_json()
+    errors = check_payload(data, ["champion"])
+    if len(errors) > 0:
+        return {"errors": errors}, 400
+    champion = str(data['champion'])
+    blb = blurb(champion)
+    return { "champion": blb }
+
+
+@app.route('/partype', methods=['GET'])
+def partype_wrapper():
+    data = request.get_json()
+    errors = check_payload(data, ["champion"])
+    if len(errors) > 0:
+        return {"errors": errors}, 400
+    champion = str(data['champion'])
+    pt = partype(champion)
+    return { "champion": pt }
