ceedcc4010014c155273289508201b7694c64f62
Wesley <0111034@student.isf.edu.hk>
2020-12-09 13:03:02 +0800

    Fixed attackrange and added movespeed function, added documentation
    What I changed
    Fixed attackrange function and added both movespeed function and more documentation
    
    What my goal was
    To fix the attackrange function and add the movespeed function, making them both work.
    I also added more documentation so its clearer what the function does

--- bot_server/services.py
+++ bot_server/services.py
@@ -8,7 +8,7 @@ services_dict = {
         "partype": [str],
         "blurb": [str],
         "title": [str],
-        "attackRange": [int],
+        "attackRange": [str],
 }
 
 def title(champion):
@@ -29,8 +29,8 @@ def title(champion):
         champion = champion.casefold() #converts string into lower case
         champion = champion.capitalize() #converts 1st letter into upper case
 
-    champion = data[champion]
-    title = champion["title"]
+    champion = data[champion] #finds dictionary of champion inside data
+    title = champion["title"] #finds dictionary of title inside champion
     return title
 
 def blurb(champion):
@@ -51,8 +51,8 @@ def blurb(champion):
         champion = champion.casefold() #converts string into lower case
         champion = champion.capitalize() #converts 1st letter into upper case
 
-    champion = data[champion]
-    blurb = champion["blurb"]
+    champion = data[champion] #finds dictionary of champion inside data
+    blurb = champion["blurb"] #finds dictionary of blurb inside champion
     return blurb
 
 def partype(champion):
@@ -73,8 +73,8 @@ def partype(champion):
         champion = champion.casefold() #converts string into lower case
         champion = champion.capitalize() #converts 1st letter into upper case
 
-    champion = data[champion]
-    partype = champion["partype"]
+    champion = data[champion] #finds dictionary of data, then champion
+    partype = champion["partype"] #finds dictionary of data, then champion, then partype
     return partype
 
 def attackrange(champion):
@@ -83,12 +83,39 @@ def attackrange(champion):
     r_json = r.json()
     data = r_json['data']
 
-#    champion = data[champion]
-    champion = data[champion[stats]]
-    #stats = data[champion[stats]]
-    attackrange = champion["attackrange"]
+    champion = champion.replace(" ","")  #replaces spaces so no edge case there
+    if champion.find("'")>= 0: #champions such as Kha'zix, Vel'koz, Cho'gath etc are sometimes spelled with an apostrophe
+        champion = champion.replace("'","")  #deletes the apostrophe
+    if champion == 'Reksai' or champion == 'Kogmaw':
+        pass
+    #nothing happens because I don't know how to generalize code for something with captial letter in the middle
+    #RekSai & KogMaw are literally the only champions with a captial letter in the middle (2/152 champions)
+    else: #for literally every champion aside from RekSai & KogMaw
+        champion = champion.casefold() #converts string into lower case
+        champion = champion.capitalize() #converts 1st letter into upper case
+
+    attackrange = data[champion]["stats"]["attackrange"] #finds dictionary of data, then champion, then stats, then attackrange
     return attackrange
 
+def movespeed(champion):
+    address = 'http://ddragon.leagueoflegends.com/cdn/10.24.1/data/en_US/champion.json'
+    r = requests.get(address)
+    r_json = r.json()
+    data = r_json['data']
+
+    champion = champion.replace(" ","")  #replaces spaces so no edge case there
+    if champion.find("'")>= 0: #champions such as Kha'zix, Vel'koz, Cho'gath etc are sometimes spelled with an apostrophe
+        champion = champion.replace("'","")  #deletes the apostrophe
+    if champion == 'RekSai' or champion == 'KogMaw':
+        pass
+    #nothing happens because I don't know how to generalize code for something with captial letter in the middle
+    #RekSai & KogMaw are literally the only champions with a captial letter in the middle (2/152 champions)
+    else: #for literally every champion aside from RekSai & KogMaw
+        champion = champion.casefold() #converts string into lower case
+        champion = champion.capitalize() #converts 1st letter into upper case
+
+    movespeed = data[champion]["stats"]["movespeed"] #finds dictionary of data, then champion, then stats, then movespeed
+    return movespeed
 # print(title("Zed"))
 # print(blurb("Zed"))
 # print(partype("Zed"))
